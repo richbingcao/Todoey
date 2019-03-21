70d81e1878e7cf54075b05a591f3c39599c9685a
Richard Cao <richardcao@richards-mbp.attlocal.net>
2019-03-21 16:01:00 -0700

    Convert to MVC design pattern.

---
+++ Todoey/Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Richard Cao on 3/20/19.
+//  Copyright © 2019 Richard Cao. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
